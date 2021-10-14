04917f2417726612351f758b4a881c0cbe0fcbb4
NadiaElso <92063948+NadiaElso@users.noreply.github.com>
2021-10-14 15:13:29 -0300

    arregluè la variable mayor o menor 20

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 let nrodeseado: number = document.getElementById("nrodeseado");
 let btn = document.getElementById("btn");
 btn.addEventListener("click", () => {
-  if (nrodeseado.value >20) {
+  if (nrodeseado.value >=20) {
     console.log("El nùmero deseado es mayor a 20":+ nrodeseado);
   } else {
     console.log("El nùmero deseado es menor o igual a 20": + nrodeseado);
